00807f0086828ff10d29fd99ccd7a4ddbeaf0a95
LEANDRO <israle_martin_25@hotmail.com>
2017-05-24 02:30:00 -0500

    Creacion perfiles

--- app/src/main/assets/initial-data.sql
+++ app/src/main/assets/initial-data.sql
@@ -4,6 +4,13 @@ INSERT INTO cuentas (id, usuario, contraseña) VALUES (2, 'Wilberth', 'wilo');
 INSERT INTO cuentas (id, usuario, contraseña) VALUES (3, 'Luis', 'lichotixko');
 
 INSERT INTO perfiles (id, usuario_id, descripcion) VALUES (0, 0, 'Perfil 1');
+INSERT INTO perfiles (id, usuario_id, descripcion) VALUES (1, 0, 'Perfil 2');
+INSERT INTO perfiles (id, usuario_id, descripcion) VALUES (2, 1, 'Perfil 5');
+INSERT INTO perfiles (id, usuario_id, descripcion) VALUES (3, 1, 'Perfil 6');
+INSERT INTO perfiles (id, usuario_id, descripcion) VALUES (4, 2, 'Perfil 12');
+INSERT INTO perfiles (id, usuario_id, descripcion) VALUES (5, 2, 'Perfil 13');
+INSERT INTO perfiles (id, usuario_id, descripcion) VALUES (6, 3, 'Perfil 56');
+INSERT INTO perfiles (id, usuario_id, descripcion) VALUES (7, 3, 'Perfil 57');
 
 INSERT INTO pin_puerta (id, usuario_id, pin) VALUES (0, 0, 1234);
 INSERT INTO pin_puerta (id, usuario_id, pin) VALUES (1, 1, 5678);
