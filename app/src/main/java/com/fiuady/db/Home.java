737919bd46b2b3cea4c5bc9938b902bf76cb4029
LEANDRO <israle_martin_25@hotmail.com>
2017-05-25 00:08:59 -0500

    id_cuenta

--- app/src/main/java/com/fiuady/db/Home.java
+++ app/src/main/java/com/fiuady/db/Home.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.CursorWrapper;
 import android.database.sqlite.SQLiteDatabase;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -146,10 +147,10 @@ public final class Home {
         return list;
     }
 
-    public int getIdCuenta(String usuario){
+    public int getIdCuenta(String usuario) {
         int id = 0;
 
-        ClassCursor cursor = new ClassCursor(db.rawQuery("SELECT id FROM cuentas WHERE usuario = '"+ usuario +"';", null));
+        ClassCursor cursor = new ClassCursor(db.rawQuery("SELECT id FROM cuentas WHERE usuario = '" + usuario + "';", null));
         while (cursor.moveToNext()) {
             id = cursor.getInt(0);
         }
@@ -160,7 +161,7 @@ public final class Home {
     public List<Perfiles> getAllPerfiles(int usuario_id) {
         ArrayList<Perfiles> list = new ArrayList<Perfiles>();
 
-        ClassCursor cursor = new ClassCursor(db.rawQuery("SELECT * FROM perfiles WHERE usuario_id = "+ usuario_id +" ORDER BY id ASC;", null));
+        ClassCursor cursor = new ClassCursor(db.rawQuery("SELECT * FROM perfiles WHERE usuario_id = " + usuario_id + " ORDER BY id ASC;", null));
         while (cursor.moveToNext()) {
             list.add(cursor.getPerfil());
         }
@@ -169,12 +170,12 @@ public final class Home {
         return list;
     }
 
-    public int getIdPerfil(String perfil,int id){
+    public int getIdPerfil(String perfil, int id) {
         int idp = 0;
 
         List<Perfiles> perfiles = getAllPerfiles(id);
-        for(Perfiles perfiles1 : perfiles){
-            if(perfiles1.getDescripcion().toUpperCase().equals(perfil.toUpperCase())){
+        for (Perfiles perfiles1 : perfiles) {
+            if (perfiles1.getDescripcion().toUpperCase().equals(perfil.toUpperCase())) {
                 idp = perfiles1.getId();
                 break;
             }
@@ -186,7 +187,7 @@ public final class Home {
     public List<Pin_puerta> getAllPines(int perfil_id) {
         ArrayList<Pin_puerta> list = new ArrayList<Pin_puerta>();
 
-        ClassCursor cursor = new ClassCursor(db.rawQuery("SELECT * FROM pin_puerta WHERE perfil_id = "+ perfil_id +" ORDER BY id ASC;", null));
+        ClassCursor cursor = new ClassCursor(db.rawQuery("SELECT * FROM pin_puerta WHERE perfil_id = " + perfil_id + " ORDER BY id ASC;", null));
         while (cursor.moveToNext()) {
             list.add(cursor.getPin_puerta());
         }
@@ -198,7 +199,7 @@ public final class Home {
     public List<Area_cochera> getAllCochera(int perfil_id) {
         ArrayList<Area_cochera> list = new ArrayList<Area_cochera>();
 
-        ClassCursor cursor = new ClassCursor(db.rawQuery("SELECT * FROM area_cochera WHERE perfil_id = "+ perfil_id +" ORDER BY id ASC;", null));
+        ClassCursor cursor = new ClassCursor(db.rawQuery("SELECT * FROM area_cochera WHERE perfil_id = " + perfil_id + " ORDER BY id ASC;", null));
         while (cursor.moveToNext()) {
             list.add(cursor.getCochera());
         }
@@ -210,7 +211,7 @@ public final class Home {
     public List<Area_frente> getAllFrente(int perfil_id) {
         ArrayList<Area_frente> list = new ArrayList<Area_frente>();
 
-        ClassCursor cursor = new ClassCursor(db.rawQuery("SELECT * FROM area_frente WHERE perfil_id = "+ perfil_id +" ORDER BY id ASC;", null));
+        ClassCursor cursor = new ClassCursor(db.rawQuery("SELECT * FROM area_frente WHERE perfil_id = " + perfil_id + " ORDER BY id ASC;", null));
         while (cursor.moveToNext()) {
             list.add(cursor.getFrente());
         }
@@ -222,7 +223,7 @@ public final class Home {
     public List<Area_hab1> getAllHabitacion1(int perfil_id) {
         ArrayList<Area_hab1> list = new ArrayList<Area_hab1>();
 
-        ClassCursor cursor = new ClassCursor(db.rawQuery("SELECT * FROM area_hab1 WHERE perfil_id = "+ perfil_id +" ORDER BY id ASC;", null));
+        ClassCursor cursor = new ClassCursor(db.rawQuery("SELECT * FROM area_hab1 WHERE perfil_id = " + perfil_id + " ORDER BY id ASC;", null));
         while (cursor.moveToNext()) {
             list.add(cursor.getHabitacion1());
         }
@@ -234,7 +235,7 @@ public final class Home {
     public List<Area_hab2> getAllHabitacion2(int perfil_id) {
         ArrayList<Area_hab2> list = new ArrayList<Area_hab2>();
 
-        ClassCursor cursor = new ClassCursor(db.rawQuery("SELECT * FROM area_hab2 WHERE perfil_id = "+ perfil_id +" ORDER BY id ASC;", null));
+        ClassCursor cursor = new ClassCursor(db.rawQuery("SELECT * FROM area_hab2 WHERE perfil_id = " + perfil_id + " ORDER BY id ASC;", null));
         while (cursor.moveToNext()) {
             list.add(cursor.getHabitacion2());
         }
@@ -258,7 +259,7 @@ public final class Home {
     public List<Area_sala> getAllSala(int perfil_id) {
         ArrayList<Area_sala> list = new ArrayList<Area_sala>();
 
-        ClassCursor cursor = new ClassCursor(db.rawQuery("SELECT * FROM area_sala WHERE perfil_id = "+ perfil_id +" ORDER BY id ASC;", null));
+        ClassCursor cursor = new ClassCursor(db.rawQuery("SELECT * FROM area_sala WHERE perfil_id = " + perfil_id + " ORDER BY id ASC;", null));
         while (cursor.moveToNext()) {
             list.add(cursor.getSala());
         }
@@ -270,7 +271,7 @@ public final class Home {
     public List<Alarmas> getAlmarmas(int perfil_id) {
         ArrayList<Alarmas> list = new ArrayList<Alarmas>();
 
-        ClassCursor cursor = new ClassCursor(db.rawQuery("SELECT * FROM alarmas WHERE perfil_id = "+ perfil_id +" ORDER BY id ASC;", null));
+        ClassCursor cursor = new ClassCursor(db.rawQuery("SELECT * FROM alarmas WHERE perfil_id = " + perfil_id + " ORDER BY id ASC;", null));
         while (cursor.moveToNext()) {
             list.add(cursor.getAlarma());
         }
@@ -279,79 +280,135 @@ public final class Home {
         return list;
     }
 
-    public void refreshCuenta(String usuario, String contraseña) {
+    public void updateCuenta(String usuario, String contraseña) {
         int idMax = 0;
         ClassCursor cursor = new ClassCursor(db.rawQuery("SELECT MAX(id) AS max FROM cuentas", null));
         while (cursor.moveToNext()) {
             idMax = cursor.getInt(0) + 1;
         }
         cursor.close();
-        db.execSQL("INSERT INTO cuentas (id, usuario, contraseña) VALUES (" + String.valueOf(idMax) + ", '" + usuario + ", "+ contraseña+"')");
+        db.execSQL("INSERT INTO cuentas (id, usuario, contraseña) VALUES (" + String.valueOf(idMax) + ", '" + usuario + ", " + contraseña + "')");
     }
 
-    public void refreshPerfil(String usuario, String contraseña) {
+    public void updatePerfil(String usuario, String contraseña) {
         int idMax = 0;
-        ClassCursor cursor = new ClassCursor(db.rawQuery("SELECT MAX(id) AS max FROM cuentas", null));
+        ClassCursor cursor = new ClassCursor(db.rawQuery("SELECT MAX(id) AS max FROM perfiles", null));
         while (cursor.moveToNext()) {
             idMax = cursor.getInt(0) + 1;
         }
         cursor.close();
-        db.execSQL("INSERT INTO cuentas (id, usuario, contraseña) VALUES (" + String.valueOf(idMax) + ", '" + usuario + ", "+ contraseña+"')");
+        db.execSQL("INSERT INTO perfiles (id, usuario_id, descripcion) VALUES (" + String.valueOf(idMax) + ", '" + usuario + ", " + contraseña + "')");
     }
 
     public void updateCochera(int perfil_id, String puerta) {
         db.execSQL("UPDATE area_cochera SET puerta =" + puerta + " WHERE perfil_id = " + String.valueOf(perfil_id));
     }
 
-    public void updateFrente(int perfil_id, String puerta, String luz, String sensor, String intensidad) {
+    public void updateFrentePuerta(int perfil_id, String puerta) {
         db.execSQL("UPDATE area_frente SET puerta =" + puerta + " WHERE perfil_id = " + String.valueOf(perfil_id));
+    }
+    public void updateFrenteLuz(int perfil_id, String luz) {
         db.execSQL("UPDATE area_frente SET luz =" + luz + " WHERE perfil_id = " + String.valueOf(perfil_id));
+    }
+    public void updateFrenteSensor(int perfil_id, String sensor) {
         db.execSQL("UPDATE area_frente SET sensor =" + sensor + " WHERE perfil_id = " + String.valueOf(perfil_id));
+
+    }
+    public void updateFrenteIntensidad(int perfil_id, String intensidad) {
         db.execSQL("UPDATE area_frente SET intensidad =" + intensidad + " WHERE perfil_id = " + String.valueOf(perfil_id));
     }
 
-    public void updateHabitacion1(int perfil_id, String luz, String rgb, String ventana, String ventilador, String autoventi, String tempmin, String tempsmax) {
+    public void updateHab1Luz(int perfil_id, String luz) {
         db.execSQL("UPDATE area_hab1 SET luz =" + luz + " WHERE perfil_id = " + String.valueOf(perfil_id));
+    }
+    public void updateHab1Rgb(int perfil_id, String rgb) {
         db.execSQL("UPDATE area_hab1 SET rgb =" + rgb + " WHERE perfil_id = " + String.valueOf(perfil_id));
+    }
+    public void updateHab1Ventana(int perfil_id, String ventana) {
         db.execSQL("UPDATE area_hab1 SET ventana =" + ventana + " WHERE perfil_id = " + String.valueOf(perfil_id));
+    }
+    public void updateHab1Ventilador(int perfil_id, String ventilador) {
         db.execSQL("UPDATE area_hab1 SET ventilador =" + ventilador + " WHERE perfil_id = " + String.valueOf(perfil_id));
+    }
+    public void updateHab1Autoventi(int perfil_id, String autoventi) {
         db.execSQL("UPDATE area_hab1 SET autoventi =" + autoventi + " WHERE perfil_id = " + String.valueOf(perfil_id));
+    }
+    public void updateHab1TempMin(int perfil_id, String tempmin) {
         db.execSQL("UPDATE area_hab1 SET tempmin =" + tempmin + " WHERE perfil_id = " + String.valueOf(perfil_id));
+    }
+    public void updateHab1TempMax(int perfil_id, String tempsmax) {
         db.execSQL("UPDATE area_hab1 SET tempsmax =" + tempsmax + " WHERE perfil_id = " + String.valueOf(perfil_id));
     }
 
-    public void updateHabitacion2(int perfil_id, String luz, String rgb, String ventana, String ventilador, String autoventi, String tempmin, String tempsmax) {
+    public void updateHab2Luz(int perfil_id, String luz) {
         db.execSQL("UPDATE area_hab2 SET luz =" + luz + " WHERE perfil_id = " + String.valueOf(perfil_id));
+    }
+    public void updateHab2Rgb(int perfil_id, String rgb) {
         db.execSQL("UPDATE area_hab2 SET rgb =" + rgb + " WHERE perfil_id = " + String.valueOf(perfil_id));
+    }
+    public void updateHab2Ventana(int perfil_id, String ventana) {
         db.execSQL("UPDATE area_hab2 SET ventana =" + ventana + " WHERE perfil_id = " + String.valueOf(perfil_id));
+    }
+    public void updateHab2Ventilador(int perfil_id, String ventilador) {
         db.execSQL("UPDATE area_hab2 SET ventilador =" + ventilador + " WHERE perfil_id = " + String.valueOf(perfil_id));
+    }
+    public void updateHab2Autoventi(int perfil_id, String autoventi) {
         db.execSQL("UPDATE area_hab2 SET autoventi =" + autoventi + " WHERE perfil_id = " + String.valueOf(perfil_id));
+    }
+    public void updateHab2TempMin(int perfil_id, String tempmin) {
         db.execSQL("UPDATE area_hab2 SET tempmin =" + tempmin + " WHERE perfil_id = " + String.valueOf(perfil_id));
+    }
+    public void updateHab2TempMax(int perfil_id, String tempsmax) {
         db.execSQL("UPDATE area_hab2 SET tempsmax =" + tempsmax + " WHERE perfil_id = " + String.valueOf(perfil_id));
     }
 
-    public void updatePatio(int perfil_id, String luzext, String sensorext, String intenext, String luzpisci, String sensorpisci, String intenpisci, String ventana) {
+    public void updatePatioLuzExt(int perfil_id, String luzext) {
         db.execSQL("UPDATE area_patio SET luzext =" + luzext + " WHERE perfil_id = " + String.valueOf(perfil_id));
+    }
+    public void updatePatioSensorExt(int perfil_id, String sensorext) {
         db.execSQL("UPDATE area_patio SET sensorext =" + sensorext + " WHERE perfil_id = " + String.valueOf(perfil_id));
+    }
+    public void updatePatioIntenExt(int perfil_id, String intenext) {
         db.execSQL("UPDATE area_patio SET intenext =" + intenext + " WHERE perfil_id = " + String.valueOf(perfil_id));
+    }
+    public void updatePatioLuzPisci(int perfil_id, String luzpisci) {
         db.execSQL("UPDATE area_patio SET luzpisci =" + luzpisci + " WHERE perfil_id = " + String.valueOf(perfil_id));
+    }
+    public void updatePatioSensorPisci(int perfil_id, String sensorpisci) {
         db.execSQL("UPDATE area_patio SET sensorpisci =" + sensorpisci + " WHERE perfil_id = " + String.valueOf(perfil_id));
-        db.execSQL("UPDATE area_patio SET intenpisci =" + intenpisci + " WHERE perfil_id = " + String.valueOf(perfil_id));
+    }
+    public void updatePatioIntenPisci(int perfil_id, String intenpisci) {
+        db.execSQL("UPDATE area_patio SET intenpisci =" + intenpisci + " WHERE perfil_id = " + String.valueOf(perfil_id));}
+    public void updatePatioVentana(int perfil_id, String ventana) {
         db.execSQL("UPDATE area_patio SET ventana =" + ventana + " WHERE perfil_id = " + String.valueOf(perfil_id));
     }
 
-    public void updateSala(int perfil_id, String puerta, String ventana, String sensormov) {
+    public void updateSalaPuerta(int perfil_id, String puerta) {
         db.execSQL("UPDATE area_patio SET puerta =" + puerta + " WHERE perfil_id = " + String.valueOf(perfil_id));
+    }
+    public void updateSalaVentana(int perfil_id, String ventana) {
         db.execSQL("UPDATE area_patio SET ventana =" + ventana + " WHERE perfil_id = " + String.valueOf(perfil_id));
+    }
+    public void updateSalaSensorMov(int perfil_id, String sensormov) {
         db.execSQL("UPDATE area_patio SET sensormov =" + sensormov + " WHERE perfil_id = " + String.valueOf(perfil_id));
     }
 
-    public void updateAlarma(int perfil_id, String puerta, String cochera, String sala, String habitacion1, String habitacion2, String sensor){
+    public void updateAlarmaPuerta(int perfil_id, String puerta) {
         db.execSQL("UPDATE area_patio SET puerta =" + puerta + " WHERE perfil_id = " + String.valueOf(perfil_id));
+    }
+    public void updateAlarmaCochera(int perfil_id, String cochera) {
         db.execSQL("UPDATE area_patio SET cochera =" + cochera + " WHERE perfil_id = " + String.valueOf(perfil_id));
+    }
+    public void updateAlarmaSala(int perfil_id, String sala) {
         db.execSQL("UPDATE area_patio SET sala =" + sala + " WHERE perfil_id = " + String.valueOf(perfil_id));
+    }
+    public void updateAlarmaHab1(int perfil_id, String habitacion1) {
         db.execSQL("UPDATE area_patio SET habitacion1 =" + habitacion1 + " WHERE perfil_id = " + String.valueOf(perfil_id));
+    }
+    public void updateAlarmaHab2(int perfil_id, String habitacion2) {
         db.execSQL("UPDATE area_patio SET habitacion2 =" + habitacion2 + " WHERE perfil_id = " + String.valueOf(perfil_id));
+    }
+    public void updateAlarmaSensor(int perfil_id, String sensor) {
         db.execSQL("UPDATE area_patio SET sensor =" + sensor + " WHERE perfil_id = " + String.valueOf(perfil_id));
     }
 }
